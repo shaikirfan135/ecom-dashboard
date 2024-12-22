bb17f78a1403fc6fcc2c9395b52311a15036ea27
irfan <shaikirfan135@gmail.com>
2024-12-22 14:04:11 +0530

    integration of register functionality

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const cors = require('cors')
+require('./db/config');
+const user = require('./db/user')
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('get successful');
+})
+
+app.post('/register', async (req, res) => {
+    console.log(req.body)
+    let userData = new user(req.body);
+    let result = await userData.save();
+    res.send(result);
 })
 
 app.listen(5000);
